62db908c492852242f9e67029714f3efd94e7c17
Daniel-Vinicius <dev.dan.programador@gmail.com>
2020-11-25 11:33:48 -0300

    Versão com README

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# Snack-Game
-Snack Game
+ <h1> Snack Game </h1>
+ <p id="sobre" align="center">
+📱 Descrição   <!-- Um emoji e uma descrição -->
+
+![](https://img.shields.io/badge/license-MIT-green)
+![](https://img.shields.io/badge/languege-Portuguese-yellow)
+[![GitHub Repo stars](https://img.shields.io/github/stars/Daniel-Vinicius/Snack-Game?style=social)](https://github.com/Daniel-Vinicius/Snack-Game/stargazers)
+
+[![Acessar](https://github.com/Daniel-Vinicius/My-Money-Frontend/blob/main/.github/acessar.png)](https://snack-game-br.vercel.app/)
+
+
+<!-- Na imagem você deve colocar um print da tela do projeto rodando, se ele tiver um front-end.
+Preferencialmente salve a imagem na pasta .github/Imagem.JPG -->
+<h2 align="center">  <img alt="Imagem do Projeto" id="imagem" title="#Projeto" src="https://github.com/Seu-perfil/repositorio/.github/Imagem.JPG" />  </h2>
+
+### Features
+
+- [x] Completo conforme o [vídeo](https://www.youtube.com/watch?v=Hua1OSXitdQ)
+- [x] Feature 2
+- [x] Feature 3
+
+<!-- Coloque o status do projeto -->
+<h4 align="left">  
+Projeto finalizado ✔
+</h4>
+
+Tabela de conteúdos
+=================
+
+<!--ts-->
+
+- [Sobre](#sobre)
+
+- [Pre Requisitos](#pre-requisitos)
+- [Executando a aplicação](#rodando)
+- [Tecnologias](#tecnologias)
+- [Autor](#autor)
+<!--te-->
+
+ <!-- Altere os Pré-requisitos -->
+
+### 🛒 Pré-requisitos<a id="pre-requisitos"></a>
+
+Antes de começar é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+
+### 📀Rodando a Aplicação<a id="rodando"></a>
+
+```bash
+ # Clone este repositório
+ git clone https://github.com/Daniel-Vinicius/Snack-Game
+
+ # Acesse a pasta do projeto no terminal
+ cd Snack-Game
+
+ # Execute a aplicação em modo de desenvolvimento
+ Acessando o navegador o [index.html](https://github.com/Daniel-Vinicius/Snack-Game/blob/main/Snack%20Game/src/index.html)
+
+```
+
+<p> Quando você tiver realizado todos os passos do projeto verá a  <a href="#imagem" >imagem do projeto</a> </p>
+
+ <!-- Altere as Tecnologias -->
+
+### 🛠 Tecnologias<a id="tecnologias"></a>
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [Css](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [Html](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+
+### 👨‍💻Autor <a id="autor"> </a>
+
+---
+
+<a href="https://github.com/Daniel-Vinicius" style="text-decoration: none;">
+<img style="border-radius: 50%;" src="https://avatars0.githubusercontent.com/u/66279500?s=460&u=03d962bd1fda436ca49d4bbfbf2f30bdd566221d&v=4" width="100px;"  alt="Seu nome"/>
+
+<br />
+<span> Feito com ❤️ por Daniel Vinicius 👋 Entre em contato! </span> 
+</a>
--- Snack Game/src/index.js
+++ Snack Game/src/index.js
@@ -1,97 +1,97 @@
 window.onload = function () {
-    var jogo = document.getElementById("jogo");
-    var contexto = jogo.getContext("2d");
-    document.addEventListener("keydown", KeyPush);
+  var jogo = document.getElementById("jogo");
+  var contexto = jogo.getContext("2d");
+  document.addEventListener("keydown", KeyPush);
 
-    setInterval(game, 80);
+  setInterval(game, 80);
 
-    const velocidade = 1;
+  const velocidade = 1;
 
-    var veloX = 0;
-    var veloY = 0;
+  var veloX = 0;
+  var veloY = 0;
 
-    var pontoX = 10;
-    var pontoY = 10;
+  var pontoX = 10;
+  var pontoY = 10;
 
-    var tamanhoPeca = 20;
-    var quantidadePeca = 30;
-    var macaX = 15;
-    var macaY = 15;
+  var tamanhoPeca = 20;
+  var quantidadePeca = 30;
+  var macaX = 15;
+  var macaY = 15;
 
-    var rastro = [];
-    cauda = 5;
+  var rastro = [];
+  cauda = 5;
 
-    function game() {
-      pontoX += veloX;
-      pontoY += veloY;
-      if (pontoX < 0) {
-        pontoX = quantidadePeca - 1;
-      }
-      if (pontoX > quantidadePeca - 1) {
-        pontoX = 0;
-      }
-      if (pontoY < 0) {
-        pontoY = quantidadePeca - 1;
-      }
-      if (pontoY > quantidadePeca - 1) {
-        pontoY = 0;
-      }
+  function game() {
+    pontoX += veloX;
+    pontoY += veloY;
+    if (pontoX < 0) {
+      pontoX = quantidadePeca - 1;
+    }
+    if (pontoX > quantidadePeca - 1) {
+      pontoX = 0;
+    }
+    if (pontoY < 0) {
+      pontoY = quantidadePeca - 1;
+    }
+    if (pontoY > quantidadePeca - 1) {
+      pontoY = 0;
+    }
 
-      contexto.fillStyle = "#9f25fa";
-      contexto.fillRect(0, 0, jogo.width, jogo.height);
+    contexto.fillStyle = "#64c4d8b9";
+    contexto.fillRect(0, 0, jogo.width, jogo.height);
 
-      contexto.fillStyle = "#2771FF";
+    contexto.fillStyle = "#FA6900";
+    contexto.fillRect(
+      macaX * tamanhoPeca,
+      macaY * tamanhoPeca,
+      tamanhoPeca,
+      tamanhoPeca
+    );
+
+    contexto.fillStyle = "#6edb4d";
+    for (var i = 0; i < rastro.length; i++) {
       contexto.fillRect(
-        macaX * tamanhoPeca,
-        macaY * tamanhoPeca,
-        tamanhoPeca,
-        tamanhoPeca
+        rastro[i].x * tamanhoPeca,
+        rastro[i].y * tamanhoPeca,
+        tamanhoPeca - 1,
+        tamanhoPeca - 1
       );
-
-      contexto.fillStyle = "#FFFFFF";
-      for (var i = 0; i < rastro.length; i++) {
-        contexto.fillRect(
-          rastro[i].x * tamanhoPeca,
-          rastro[i].y * tamanhoPeca,
-          tamanhoPeca - 1,
-          tamanhoPeca - 1
-        );
-        if (rastro[i].x === pontoX && rastro[i].y === pontoY) {
-          veloY = 0;
-          veloX = 0;
-          cauda = 5;
-        }
-      }
-      rastro.push({ x: pontoX, y: pontoY });
-      while (rastro.length > cauda) {
-        rastro.shift();
-      }
-
-      if (macaX == pontoX && macaY == pontoY) {
-        cauda++;
-        macaX = Math.floor(Math.random() * quantidadePeca);
-        macaY = Math.floor(Math.random() * quantidadePeca);
+      if (rastro[i].x === pontoX && rastro[i].y === pontoY) {
+        veloY = 0;
+        veloX = 0;
+        cauda = 5;
       }
     }
-
-    function KeyPush(event) {
-      switch (event.keyCode) {
-        case 37: // left
-          veloX = -velocidade;
-          veloY = 0;
-          break;
-        case 38: // up
-          veloX = 0;
-          veloY = -velocidade;
-          break;
-        case 39: // right
-          veloX = velocidade;
-          veloY = 0;
-          break;
-        case 40: // down
-          veloX = 0;
-          veloY = velocidade;
-          break;
-      }
+    rastro.push({ x: pontoX, y: pontoY });
+    while (rastro.length > cauda) {
+      rastro.shift();
     }
-  };
+
+    if (macaX == pontoX && macaY == pontoY) {
+      cauda++;
+      macaX = Math.floor(Math.random() * quantidadePeca);
+      macaY = Math.floor(Math.random() * quantidadePeca);
+    }
+  }
+
+  function KeyPush(event) {
+    switch (event.keyCode) {
+      case 37: // left
+        veloX = -velocidade;
+        veloY = 0;
+        break;
+      case 38: // up
+        veloX = 0;
+        veloY = -velocidade;
+        break;
+      case 39: // right
+        veloX = velocidade;
+        veloY = 0;
+        break;
+      case 40: // down
+        veloX = 0;
+        veloY = velocidade;
+        break;
+    }
+  }
+};
